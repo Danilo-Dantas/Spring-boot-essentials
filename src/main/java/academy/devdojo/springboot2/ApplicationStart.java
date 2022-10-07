16f27b2f016e3aed63e51b355f5168589b9ccb73
Danilo <danilodantasofc@gmail.com>
2022-10-07 10:09:57 -0300

    criando DateUtil

--- src/main/java/academy/devdojo/springboot2/ApplicationStart.java
+++ src/main/java/academy/devdojo/springboot2/ApplicationStart.java
@@ -1,11 +1,11 @@
-package academy.devdojo.springboot2.start;
+package academy.devdojo.springboot2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
-@ComponentScan(basePackages = "academy.devdojo.springboot2")
+@ComponentScan
 public class ApplicationStart {
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationStart.class, args);
--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -2,16 +2,20 @@ package academy.devdojo.springboot2.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.util.DateUtil;
 
 @RestController
 @RequestMapping("anime")
 public class AnimeController {
-	
+	@Autowired
+	private DateUtil dateUtil;
+	//localhost:8080/anime/list
 	@GetMapping(path = "list")
 	public List<Anime> list(){
 		return List.of(new Anime("DBZ"), new Anime("Berserk"));
